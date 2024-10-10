412ae501843eaa403c3feeac1d1bf7ceb9e34a4f
Garett Bouline <gbouline@cisco.com>
2024-10-09 19:22:45 -0600

    run scrip 2 via bash

--- 1-install.sh
+++ 1-install.sh
@@ -72,5 +72,5 @@ cp snapshot.sh /mnt/archinstall/
 # ------------------------------------------------------
 # Chroot to installed sytem
 # ------------------------------------------------------
-arch-chroot /mnt ./archinstall/2-configuration.sh
+arch-chroot /mnt /bin/bash ./archinstall/2-configuration.sh
 
