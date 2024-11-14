82c0b55d538469aca86494ec0c481387c2d77de1
Garett Bouline <gbouline@cisco.com>
2024-11-14 11:02:54 -0600

    add break after grub-install to confirm or quit

--- 2-configuration.sh
+++ 2-configuration.sh
@@ -90,6 +90,8 @@ systemctl enable acpid
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 
+read -p "Confirm grub install was succesful Enter to continue Ctrl-C to quit..."
+
 # ------------------------------------------------------
 # Add btrfs and setfont to mkinitcpio
 # ------------------------------------------------------
