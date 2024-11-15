6860765737db2ee1a3a06c4bdfec70573ff9830c
Garett Bouline <gbouline@cisco.com>
2024-11-15 17:04:10 -0600

    remove --removeable from grub-install

--- 2-configuration.sh
+++ 2-configuration.sh
@@ -87,7 +87,7 @@ systemctl enable acpid
 # ------------------------------------------------------
 # Grub installation
 # ------------------------------------------------------
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 read -p "Confirm grub install was succesful Enter to continue Ctrl-C to quit..."
